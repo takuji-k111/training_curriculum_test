c4cac112267ec1ec9b67eeb5b063be745ec230c3
takuji-k111 <64259726+takuji-k111@users.noreply.github.com>
2020-11-02 08:05:10 +0900

    Revert "Revert "ISSUE4の修正""
    This reverts commit 5c3ed525d8fdf89538c94c237825d4ae58b58b93.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,9 +36,15 @@ class CalendarsController < ApplicationController
       end
       wday_num = Date.today.wday
       if wday_num >= 7
+<<<<<<< Updated upstream
         wday_num = wday_num -7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
+=======
+        wday_num = wday_num-7
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day,  wday: wdays[wday_num], plans: today_plans}
+>>>>>>> Stashed changes
       @week_days.push(days)
     end
 
